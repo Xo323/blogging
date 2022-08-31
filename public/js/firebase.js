b02a597eec348135777a91e5567bc3c6fcfadd62
Xo323 <102453435+Xo323@users.noreply.github.com>
2022-08-31 20:35:19 +0530

    update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,4 +1,11 @@
 let firebaseConfig = {
+  
+  apiKey: "AIzaSyCqbPT4SGpenfl-IC2ZIAOXCFPQkL64AAc",
+  authDomain: "easyblogs-6c2d8.firebaseapp.com",
+  projectId: "easyblogs-6c2d8",
+  storageBucket: "easyblogs-6c2d8.appspot.com",
+  messagingSenderId: "898336979518",
+  appId: "1:898336979518:web:c884bdc46dc58e9c30af6f"
     // Enter your firebase credentials
 };
 
